90757e331fb9c7b339bb5018d15059d957135033
richheadco <richheadco@users.noreply.github.com>
2021-07-08 11:54:09 +0900

    setting files

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 // @ts-check
-// Formatting, Linting, Type Checking
-// Formatting: Prettier
-// Linting: ESLint
-// Type Checking: TypeScript
 
-const http = require('http')
+// 프레임워크 없이 간단한 토이 프로젝트 웹 서버 만들어보기
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.end('Hello!')
-})
-
-const PORT = 4000
-server.listen(PORT, () => {
-    console.log('The server is listening at port: ' + PORT)
-})
+/**
+ * 블로그 포스팅 서비스
+ * - 로컬 파일을 데이터베이스로 활용할 예정(JSON)
+ * - 인증 로직은 넣지 않는다.
+ * - RESTful API를 사용한다.
+ */
